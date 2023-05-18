e60c16d06c6fc23bd15a7e9eef7ed6f912c456a4
Patel Henil <76421423+HenilX@users.noreply.github.com>
2023-05-18 10:08:19 +0000

    #ok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import { BrowserRouter, Route, Router, Routes,} from 'react-router-dom';
 import Login from './Page/Login';
+import ProductList from './Page/ProductList';
+import Register from './Page/Register.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> 
       <Login/>     
+      <ProductList/>
+      <Register/>
          <BrowserRouter>
          <Routes>
           <Route path="/Login" element={Login} />
+          <Route path="/ProductList" element={ProductList}/>
+          <Route Path="/Register" element = {Register}/>
          </Routes>
          </BrowserRouter>
       </header>
--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../Component/Header'
-import Footer from '../Component/footer'
+import Footer from '../Component/Footer'
 
 function Login() {
   return (
