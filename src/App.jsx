48569242c1305b37ebcd9fee36e3a3926428451a
Patel Henil <76421423+HenilX@users.noreply.github.com>
2023-05-19 09:06:45 +0000

    React app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Page/Login";
+import ProductList from "./Page/ProductList";
+import Register from "./Page/Register.jsx";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <div className="name"></div>
+        <Login />
+        
+        <Register/>
+        <Routes>
+          <Route path="/Login" element={Login} />
+          <Route path="/ProductList" element={ProductList} />
+          <Route Path="/Register" element={Register} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
