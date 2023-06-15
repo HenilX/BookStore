0be226aa78ef32f1c7cdbd1a892962450f7e1fed
HenilX <henil59999@gmail.com>
2023-06-15 11:40:28 +0530

    Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./Page/Login";
-import ProductList from "./Page/ProductList";
-import Register from "./Page/Register.jsx";
+import React from "react";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./utils/theme";
+import "./assets/css/style.css";
+import Header from "./components/header/index";
+import Footer from "./components/footer/index";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MainNavigation from "./components/MainNavigation";
+import { AuthWrapper } from "./context/auth";
+import loader from "../src/assets/images/loader.gif";
+import { CartWrapper } from "./context/cart";
+import store from "./State/store";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Login />
-
-        <Register />
-        <Routes>
-          <Route path="/Login" element={Login} />
-          <Route path="/ProductList" element={ProductList} />
-          <Route Path="/Register" element={Register} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <ThemeProvider theme={theme}>
+      <React.Suspense fallback={<></>}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <AuthWrapper>
+              <CartWrapper>
+                <div className="loader-wrapper">
+                  <img src={loader} alt="loader" />
+                </div>
+                <div className="wrapper">
+                  <Header />
+                  <main>
+                    <MainNavigation />
+                  </main>
+                  <Footer />
+                </div>
+                <ToastContainer />
+              </CartWrapper>
+            </AuthWrapper>
+          </BrowserRouter>
+        </Provider>
+      </React.Suspense>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
